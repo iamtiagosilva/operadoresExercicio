0932e69616caf4d6d28f29826aef8f2add91d901
Talentos C08100O <iamtiagosilva>
2023-04-27 08:52:34 -0300

    exercicio

---
+++ index.js
@@ -0,0 +1,90 @@
+//console.log("hello")
+// comentario
+//prompt("Digite seu nome?")
+
+
+
+// const idade = prompt("Digite sua idade?")
+// console.log(idade)
+
+// const nome = prompt("Digite seu nome?")
+// console.log(nome)
+
+// const endereco = prompt("Digite seu endereço?")
+// console.log (endereco)
+
+
+ // let anoNascimento = 2007
+
+ // anoNascimento = 2023 - anoNascimento
+
+ // console.log(anoNascimento) 
+
+ // const nome = prompt("Digite seu nome?")
+
+ // const idade =  prompt("Digite sua idade?")
+
+// console.log("Olá!", "Meu nome é", nome, "e eu tenho" , idade, "anos")
+
+
+//const got = "Game of Thrones"
+//const temporadasDeGot = 8
+
+//console.log(typeof got)
+//console.log(typeof temporadasDeGot)
+
+//let endereco
+//console.log(endereco)
+
+//let idade = prompt("Digite sua idade")
+//idade = idade + 10
+//console.log("sua idade daqui a 10 anos é:", idade)
+
+
+//let nome = prompt("Digite seu nome")
+
+//console.log(typeof nome)
+
+//let idade = prompt("Digite sua idade")
+
+//console.log(typeof idade)
+
+//let nome
+//let idade
+
+//console.log(typeof nome);
+//console.log(typeof idade);
+
+//nome = String(prompt("informe seu nome"))
+//idade = Number(prompt("informe sua idade:"))
+
+//console.log(typeof nome);
+//console.log(typeof idade);
+
+//console.log("Olá", nome, "sua idade é:", idade)
+
+//let a = 10
+//let b = 10
+//c = a
+//b = c
+//a = b 
+
+//console.log(a, b, c)
+
+//let p = prompt("Quantas horas você trabalha por dia?")
+//let t = prompt("Quanto você recebe por dia?")
+//alert(`Voce recebe ${t/p} por hora`)
+
+//const pergunta = prompt("voce comeu hoje? (sim/nao)")
+
+//console.log("Primeira pergunta: ", pergunta);
+
+let a = Number(prompt("digite um numero"))
+let b = Number(prompt("digite outro numero"))
+
+let c = b
+b = a
+a = c
+
+console.log("Valor do primeiro numero" ,a);
+console.log("Valor do segundo numero" ,b);
